9b8215a8c69f672fbb252c0f00d7e0a2b7a70d9b
devlaps-co <pandit.alabi@devlaps.co>
2024-06-20 22:22:27 +0000

    feat: Hero section complete without get stated button routing

--- src/app/features/landingpage/components/hero/hero.component.ts
+++ src/app/features/landingpage/components/hero/hero.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { NgxGlideModule } from 'ngx-glide';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [NgxGlideModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HeroComponent {
 
+  glideOptions = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    autoplay: 3000,
+  };
+
+  listOfCards : Array<any> = [
+    {
+      title : "Title 1",
+      subtitle : "Somethings about title 1"
+    },
+    {
+      title : "Title 2",
+      subtitle : "Somethings about title 2"
+    },
+    {
+      title : "Title 3",
+      subtitle : "Somethings about title 3"
+    },
+    {
+      title : "Title 4",
+      subtitle : "Somethings about title 4"
+    },
+    {
+      title : "Title 5",
+      subtitle : "Somethings about title 5"
+    },
+  ]
 }
--- src/app/features/landingpage/landingpage.component.ts
+++ src/app/features/landingpage/landingpage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../core/components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet, FooterComponent],
+  imports: [NavbarComponent, HeroComponent, RouterOutlet, FooterComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
