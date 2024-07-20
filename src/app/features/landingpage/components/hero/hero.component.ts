ad0df90632d1db5e2320150e8649d473991a3024
PanditAlabi <pandit.alabi@amalitech.com>
2024-07-20 11:14:06 +0000

    chore: adding dummy profile pictures

--- src/app/features/landingpage/components/hero/hero.component.ts
+++ src/app/features/landingpage/components/hero/hero.component.ts
@@ -40,4 +40,11 @@ export class HeroComponent {
       subtitle : "Somethings about title 5"
     },
   ]
+
+  TopRetailers : Array<string> = [
+     "https://img.freepik.com/free-photo/middle-aged-cheerful-dark-skinned-male-with-shining-smile_273609-28538.jpg?ga=GA1.2.1900006000.1721461402&semt=sph",
+     "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?t=st=1721473657~exp=1721477257~hmac=d8a94b02718bee348f40461cbf3ea06ace3c6273a478f1144dd2e6eb1f6b03ff&w=740",
+     "https://img.freepik.com/free-photo/confident-african-businesswoman-smiling-closeup-portrait-jobs-career-campaign_53876-143280.jpg?ga=GA1.1.1900006000.1721461402&semt=sph",
+     "https://img.freepik.com/premium-photo/smiling-asian-man-standing-with-hands-folded-concept-engineering-jobs_264197-8821.jpg?ga=GA1.1.1900006000.1721461402&semt=sph"
+  ]
 }
