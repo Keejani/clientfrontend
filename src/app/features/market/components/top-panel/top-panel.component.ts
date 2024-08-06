af924e7c8598d7f8c67b56871e387b6c21aad456
PanditAlabi <pandit.alabi@amalitech.com>
2024-08-06 11:02:15 +0000

    feat: top-panel ui

---
+++ src/app/features/market/components/top-panel/top-panel.component.ts
@@ -0,0 +1,95 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-top-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-panel.component.html',
+  styleUrl: './top-panel.component.css'
+})
+export class TopPanelComponent implements AfterViewInit {
+
+  savedRetailers: Array<any> = [
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+    {
+      name: "AMSA",
+      owner: "Someone",
+      image: "https://img.freepik.com/premium-photo/sawdust_15578-97.jpg?w=1060"
+    },
+  ]
+
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+
+  private isDown = false;
+  private startX = 0;
+  private scrollLeft = 0;
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.scrollContainer.nativeElement.addEventListener('wheel', (event: WheelEvent) => {
+      event.preventDefault();
+    });
+
+    this.scrollContainer.nativeElement.addEventListener('mousedown', (e: MouseEvent) => {
+      this.isDown = true;
+      this.startX = e.pageX - this.scrollContainer.nativeElement.offsetLeft;
+      this.scrollLeft = this.scrollContainer.nativeElement.scrollLeft;
+      this.scrollContainer.nativeElement.classList.add('active');
+    });
+
+    this.scrollContainer.nativeElement.addEventListener('mouseleave', () => {
+      this.isDown = false;
+      this.scrollContainer.nativeElement.classList.remove('active');
+    });
+
+    this.scrollContainer.nativeElement.addEventListener('mouseup', () => {
+      this.isDown = false;
+      this.scrollContainer.nativeElement.classList.remove('active');
+    });
+
+    this.scrollContainer.nativeElement.addEventListener('mousemove', (e: MouseEvent) => {
+      if (!this.isDown) return;
+      e.preventDefault();
+      const x = e.pageX - this.scrollContainer.nativeElement.offsetLeft;
+      const walk = (x - this.startX) * 3; // Adjust the multiplier to speed up or slow down the scroll
+      this.scrollContainer.nativeElement.scrollLeft = this.scrollLeft - walk;
+    });
+  }
+
+
+}
--- src/app/features/market/main/main.component.ts
+++ src/app/features/market/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../core/components/navbar/navbar.component";
 import { ProductsFetchComponent } from "../products-fetch/products-fetch.component";
+import { TopPanelComponent } from "../components/top-panel/top-panel.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavbarComponent, ProductsFetchComponent],
+  imports: [NavbarComponent, ProductsFetchComponent, TopPanelComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
