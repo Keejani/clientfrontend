ba5ecd61656212067abd297099779217016d95de
PanditAlabi <pandit.alabi@amalitech.com>
2024-08-11 21:15:48 +0000

    feat: cart component ui

--- src/app/features/market/cart/cart.component.ts
+++ src/app/features/market/cart/cart.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../core/components/navbar/navbar.component";
+import { TopPanelComponent } from "../components/top-panel/top-panel.component";
+import { ProductsFetchComponent } from "../products-fetch/products-fetch.component";
+import { SubTotalComponent } from "./components/sub-total/sub-total.component";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, TopPanelComponent, ProductsFetchComponent, SubTotalComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
