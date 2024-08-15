4500de79227629027b9786feae712eee52f5e34a
devlaps-co <pandit.alabi@devlaps.co>
2024-08-15 04:03:51 +0000

    feat: cart-cummary sub-total

--- src/app/features/market/cart/components/sub-total/sub-total.component.ts
+++ src/app/features/market/cart/components/sub-total/sub-total.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { GeneralbuttonComponent } from "../../../../components/generalbutton/generalbutton.component";
 
 @Component({
   selector: 'app-sub-total',
   standalone: true,
-  imports: [],
+  imports: [GeneralbuttonComponent],
   templateUrl: './sub-total.component.html',
   styleUrl: './sub-total.component.css'
 })
 export class SubTotalComponent {
+
+  buttonStyle = {
+    'width' : '100%',
+    'height' : '50px',
+    'background-color' : 'var(--primaryAccentColor)',
+    'display' : 'flex',
+    'justify-content' : 'center',
+    'align-items' : 'center',
+    'padding' : '0 20px',
+    'color' : '#fff',
+    'font-weight' : '600',
+    'margin-block-start': '30px'
+  }
 
 }
