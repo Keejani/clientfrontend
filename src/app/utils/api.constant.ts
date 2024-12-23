d7540155dcb2b6c9ef57073736c87c62e82f9979
devlaps-co <pandit.alabi@devlaps.co>
2024-12-23 12:29:33 +0000

    feat: changed api base url

--- src/app/utils/api.constant.ts
+++ src/app/utils/api.constant.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3000'
-// export const API_BASE_URL = 'http://api.keejani.com'
+// export const API_BASE_URL = 'http://localhost:3000'
+export const API_BASE_URL = 'http://api.keejani.com'
 
 export const OAuth2Google = API_BASE_URL + `/api/v1/auth/oauth2/google`;
